66c654c7e2b5d7ff2665d01afb41227336145421
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-07-05 15:27:45 +0200

    bug quand on veux afficher une page encodée dans un navigateur ( le decodage ne se fait pas : trouver pourquoi )

--- classes/HtmlSecure.php
+++ classes/HtmlSecure.php
@@ -3,54 +3,115 @@
 class HtmlSecure {
 
 	private static $base64_cache_dir = './cache';
+	private static $extention = '.64';
 
 	private $liste_pages;
 	public function __construct(array $liste_pages = []) {
 		$this->liste_pages = $liste_pages;
 	}
-
-	public function genere_base64_version($nb = 1) {
-		foreach ($this->liste_pages as $path) {
-			self::genere_base64($path, $nb);
+	public function get_all_files_of_dir($path = '.') {
+		if(is_dir($path)) {
+			$dir = opendir($path);
+			while (($file = readdir($dir)) !== false) {
+				if($file !== '.' && $file !== '..') {
+					if(is_file("{$path}/{$file}")) {
+						$this->liste_pages[] = "{$path}/{$file}";
+					}
+					if(is_dir("{$path}/{$file}")) {
+						$this->get_all_files_of_dir("{$path}/{$file}");
+					}
+				}
+			}
 		}
 	}
-
-	public static function genere_base64($path, $nb = 1) {
-		if(!is_dir(self::$base64_cache_dir)) {
-			mkdir('./cache', 0777, true);
+	public function genere_base64_version($nb_encode = 1) {
+		foreach ($this->liste_pages as $path) {
+			self::genere_base64($path, $nb_encode);
 		}
-
+	}
+	public static function genere_base64($path, $nb_encode = 1) {
+		if(!is_dir(self::$base64_cache_dir)) {
+			mkdir(self::$base64_cache_dir, 0777, true);
+		}
 		if(is_file($path)) {
-			$base64_filename = str_replace('.html', '', basename($path));
-			for($i = 0, $max = $nb; $i<$max; $i++)
+			$base64_filename = $path;
+			for($i = 0, $max = $nb_encode; $i < $max; $i++)
 				$base64_filename = base64_encode($base64_filename);
 			$base64_content = file_get_contents($path);
-			for($i = 0, $max = $nb; $i<$max; $i++)
+			for($i = 0, $max = $nb_encode; $i < $max; $i++)
 				$base64_content = base64_encode($base64_content);
 			file_put_contents(self::$base64_cache_dir."/{$base64_filename}.64", $base64_content);
+			unlink($path);
+		}
+		$nb_file = 0;
+		$directory = str_replace('/'.basename($path), '', $path);
+		if(is_dir($directory)) {
+			$dir     = opendir($directory);
+			while (($file = readdir($dir)) !== false) {
+				if ($file !== '.' && $file !== '..' && is_file($path)) {
+					$nb_file++;
+				}
+			}
+		}
+		if($nb_file === 0) {
+			rmdir($directory);
 		}
 	}
-
-	public function get_html_version($nb = 1): array {
+	public function get_html_version($nb_encode = 1): array {
 		$liste_html = [];
 		foreach ($this->liste_pages as $path) {
-			$liste_html[] = self::get_html($path, $nb);
+			$liste_html[] = self::get_html($path, $nb_encode)[0];
 		}
 
 		return $liste_html;
 	}
-
-	public static function get_html($path, $nb = 1): string {
-		$filename = str_replace('.html', '', basename($path));
-		for($i = 0, $max = $nb; $i<$max; $i++)
-			$base64_filename = base64_encode($filename);
-		if(is_file(self::$base64_cache_dir."/{$base64_filename}.64")) {
-			$base64_content = file_get_contents(self::$base64_cache_dir."/{$base64_filename}.64");
-			$html_content = $base64_content;
-			for($i = 0, $max = $nb; $i<$max; $i++)
-				$html_content = base64_decode($html_content);
-			return $html_content;
+	public static function get_html($path, $nb_encode = 1): array {
+		$base64_filename = $path;
+		for($i = 0, $max = $nb_encode; $i < $max; $i++) {
+			$base64_filename = base64_encode($base64_filename);
 		}
-		return '';
+		if(is_file(self::$base64_cache_dir.'/'.$base64_filename.self::$extention)) {
+			$base64_content = file_get_contents(self::$base64_cache_dir."/{$base64_filename}".self::$extention);
+			$html_content = $base64_content;
+			for($i = 0, $max = $nb_encode; $i < $max; $i++) {
+				$html_content = base64_decode($html_content);
+			}
+			return [
+				$html_content,
+				$path
+			];
+		}
+		return [
+			'',
+			''
+		];
+	}
+	public function recreate_html_page($nb_encode = 1) {
+		$liste_pages = [];
+		$this->liste_pages = [];
+		$this->get_all_files_of_dir('./cache');
+		foreach ($this->liste_pages as $path) {
+			if($path_regenerated = self::recreate_html($path, $nb_encode, true)) {
+				$liste_pages[] = $path_regenerated;
+			}
+		}
+		return $liste_pages;
+	}
+	public static function recreate_html($path, $nb_encode = 1, $base64_title = false) {
+		if($base64_title) {
+			$path = str_replace([self::$extention, self::$base64_cache_dir.'/'], '', $path);
+			for($i = 0, $max = $nb_encode; $i<$max; $i++) {
+				$path = base64_decode($path);
+			}
+		}
+		list($content, $path_regenerated) = self::get_html($path, $nb_encode);
+		if(!is_dir(str_replace('/'.basename($path), '', $path))) {
+			mkdir(str_replace('/'.basename($path), '', $path), 0777, true);
+		}
+		if($path_regenerated !== '') {
+			file_put_contents($path_regenerated, $content);
+			return $path_regenerated;
+		}
+		return null;
 	}
 }
--- demo.php
+++ demo.php
@@ -2,6 +2,18 @@
 
 require_once 'classes/HtmlSecure.php';
 
-$html_secure = new HtmlSecure(['./website/index.html',]);
-$html_secure->genere_base64_version(1);
-foreach ($html_secure->get_html_version(1) as $page) echo $page."\n";
+$html_secure = new HtmlSecure();
+$html_secure->get_all_files_of_dir('./website');
+$html_secure->genere_base64_version(5);
+if(isset($_SERVER['REQUEST_URI'])) {
+	$filename = basename(__FILE__);
+	var_dump('./website'.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']));
+	echo HtmlSecure::get_html('./website'.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']), 5)[0];
+}
+elseif (isset($argv[1])) {
+	var_dump('./website'.$argv[1]);
+	echo HtmlSecure::get_html('./website'.$argv[1], 5)[0];
+}
+//foreach ($html_secure->recreate_html_page(5) as $file) {
+//	echo "Le fichier {$file} à été regénéré avec succes !\n";
+//}
