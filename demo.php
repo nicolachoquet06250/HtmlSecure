cb5c4abef75bfcd81644ca6bff012c6ffc1325c5
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-07-09 12:07:35 +0200

    reglage des bugs

--- demo.php
+++ demo.php
@@ -2,18 +2,17 @@
 
 require_once 'classes/HtmlSecure.php';
 
+$nb_encode = 5;
+
 $html_secure = new HtmlSecure();
 $html_secure->get_all_files_of_dir('./website');
-$html_secure->genere_base64_version(5);
+$html_secure->genere_base64_version($nb_encode);
 if(isset($_SERVER['REQUEST_URI'])) {
 	$filename = basename(__FILE__);
-	var_dump('./website'.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']));
-	echo HtmlSecure::get_html('./website'.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']), 5)[0];
+	echo HtmlSecure::get_html('./website'.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']), $nb_encode)[0];
 }
-elseif (isset($argv[1])) {
-	var_dump('./website'.$argv[1]);
-	echo HtmlSecure::get_html('./website'.$argv[1], 5)[0];
+elseif (isset($argv[1]) && $argv[1] === 'regenere') {
+	foreach ($html_secure->recreate_html_page($nb_encode) as $file) {
+		echo "Le fichier {$file} à été regénéré avec succes !\n";
+	}
 }
-//foreach ($html_secure->recreate_html_page(5) as $file) {
-//	echo "Le fichier {$file} à été regénéré avec succes !\n";
-//}
