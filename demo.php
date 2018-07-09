102bb67fe0f5926945572b57f76ae3f2c58a37f6
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2018-07-09 14:06:23 +0200

    reglage des bugs

--- demo.php
+++ demo.php
@@ -3,13 +3,14 @@
 require_once 'classes/HtmlSecure.php';
 
 $nb_encode = 5;
+$path = './website';
 
 $html_secure = new HtmlSecure();
-$html_secure->get_all_files_of_dir('./website');
+$html_secure->get_all_files_of_dir($path);
 $html_secure->genere_base64_version($nb_encode);
 if(isset($_SERVER['REQUEST_URI'])) {
 	$filename = basename(__FILE__);
-	echo HtmlSecure::get_html('./website'.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']), $nb_encode)[0];
+	echo HtmlSecure::get_html($path.str_replace('/'.$filename, '', $_SERVER['REQUEST_URI']), $nb_encode)[0];
 }
 elseif (isset($argv[1]) && $argv[1] === 'regenere') {
 	foreach ($html_secure->recreate_html_page($nb_encode) as $file) {
